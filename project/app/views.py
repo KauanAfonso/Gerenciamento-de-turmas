1ac0160f578ea782f8a7fb3d2233165a50da9cc8
kauanAfonso <kauanafon3@gmail.com>
2025-03-18 11:30:49 -0300

    tratando erros

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect , get_object_or_404, get_list_or_404
+from django.shortcuts import render,redirect , get_object_or_404
 from .models import *
 from django.http import HttpResponseNotFound
 from .forms import *
@@ -9,8 +9,9 @@ from .forms import *
 def mostrar_turmas(request):
     try:
         turma = Turma.objects.all()
-    except:
-        return "erro"
+    except Exception as e:
+        print(f"erro: {e}")
+        return HttpResponseNotFound('Erro ao tentar carregar os professores')
     return render(request, "turmas.html" , {"turmas": turma})
 
 
@@ -50,61 +51,78 @@ def visualizar_sala(request, pk):
 
 
 def visualizar_turmas_professor(request, pk):
-    professor = get_object_or_404(Professor,pk=pk)
-    aulas = Aulas.objects.filter(pk_professor =professor)
-    professor_aulas = []
-    
-    for aula in aulas:
-        professor_aulas.append(
-            {
-                'turma': aula.pk_turma,
-                'materia': aula.pk_materia,
-                'horario': aula.pk_horario,
-                'dia_semana': aula.pk_dias_semana
-            }
-        )
-    return render(request, 'prof_detalhes.html' , {'professores':professor_aulas})
+    try:
+        professor = get_object_or_404(Professor,pk=pk)
+        aulas = Aulas.objects.filter(pk_professor =professor)
+        professor_aulas = []
+        
+        for aula in aulas:
+            professor_aulas.append(
+                {
+                    'turma': aula.pk_turma,
+                    'materia': aula.pk_materia,
+                    'horario': aula.pk_horario,
+                    'dia_semana': aula.pk_dias_semana
+                }
+            )
+        return render(request, 'prof_detalhes.html' , {'professores':professor_aulas})
+    except Exception as e:
+        print(f"Error: {e}")
+        
+        return HttpResponseNotFound("Ocorreu um erro ao tentar carregar a turma e os professores.") #mensagem de erro caso for nescessario
 
 
 
 ''''
 
------------------------------------PARTE DO ADMINISTRADOR-------------
+-----------------------------------PARTE DO ADMINISTRADOR----------------------------
 
 '''
 
 def criar_aula(request,pk):
-    if request.method == 'POST':
-        formulario = AulasForm(request.POST)
+    try:
+        if request.method == 'POST':
+            formulario = AulasForm(request.POST)
 
-        if formulario.is_valid():
-            formulario.save()
-            return redirect("detalhes.html") 
-            
-    else:
-        formulario = AulasForm(initial={'pk_turma':pk})
-    return render(request, 'form_aula.html', {'form':formulario})           
+            if formulario.is_valid():
+                formulario.save()
+                return redirect("detalhes.html") 
+                
+        else:
+            formulario = AulasForm(initial={'pk_turma':pk})
+        return render(request, 'form_aula.html', {'form':formulario, "mensagem":"Criar essa aula?"})
+    except Exception as e:
+        print('erro' + e)
+        return HttpResponseNotFound("Erro ao tentar criar aula.")            
 
 
 def deletar_aula(request, pk):
-    aula = get_object_or_404(Aulas,pk=pk)
+    try:
+        aula = get_object_or_404(Aulas,pk=pk)
 
-    if request.method == "POST":
-        aula.delete()
-        return redirect("mostrar_turmas")
-    else:
-        return render(request, "excluir_aula.html", {"aula":aula})
-    
+        if request.method == "POST":
+            aula.delete()
+            return redirect("mostrar_turmas")
+        else:
+            return render(request, "excluir_aula.html", {"aula":aula})
+    except Exception as e:
+        print("ERRO" + e)
+        return HttpResponseNotFound("Erro ao tetnar deletar a aula.")  
+
 
 def atualizar_aula(request,pk):
-    aula = get_object_or_404(Aulas, pk=pk)
-    if request.method == 'POST':
-        formulario = AulasForm(request.POST, instance=aula)
+    try:
+        aula = get_object_or_404(Aulas, pk=pk)
+        if request.method == 'POST':
+            formulario = AulasForm(request.POST, instance=aula)
 
-        if formulario.is_valid():
-            formulario.save()
-            return redirect("detalhes.html") 
-            
-    else:
-        formulario = AulasForm(instance=aula)
-    return render(request, 'form_aula.html', {'form':formulario})     
+            if formulario.is_valid():
+                formulario.save()
+                return redirect("detalhes.html") 
+                
+        else:
+            formulario = AulasForm(instance=aula)
+        return render(request, 'form_aula.html', {'form':formulario, "mensagem":"Atualizar essa aula?"})
+    except Exception as e:
+        print("Erro" + e)
+        return HttpResponseNotFound("Erro ao tentar atulizar a aula")     
