84556c6f86efe648b9afd1a4d6417727e1107b56
kauanAfonso <kauanafon3@gmail.com>
2025-03-21 08:19:01 -0300

    Lidando com o form do aluno

--- project/app/forms.py
+++ project/app/forms.py
@@ -15,7 +15,24 @@ class AlunoForm(forms.ModelForm):
     class Meta:
         model = Aluno
         pk_turma = forms.IntegerField(label='aa', required=False)  # Adicionando o campo dentro da classe meta para que o valor padrão ja apareça la
-        fields = '__all__'
+        fields = ['username', 'email', 'first_name', 'last_name', 'ra', 'data_nascimento',  'pk_turma', 'foto_perfil', 'is_active']
+
+        labels = {
+            'username': 'Nome de Usuário',
+            'email': 'Endereço de E-mail',
+            'first_name': 'Primeiro Nome',
+            'last_name': 'Sobrenome',
+            'ra': 'Número de RA',
+            'data_nascimento': 'Data de Nascimento',
+            'pk_turma': 'Turma',
+            'foto_perfil': 'Foto de Perfil',
+            'is_active': 'Ativo'
+        }
+
+        widgets = {
+            'data_nascimento': forms.DateInput(attrs={'type': 'date'}),  # Usando DateInput para o campo de data
+            
+        }
 
 class AulasForm(forms.ModelForm):
    
