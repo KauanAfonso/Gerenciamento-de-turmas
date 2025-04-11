a6d66fd83c40fd3006410310880dae82be050fc6
kauanAfonso <kauanafon3@gmail.com>
2025-04-11 09:57:53 -0300

    Estilizando o front

--- project/project/settings.py
+++ project/project/settings.py
@@ -125,8 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'app/static']
+
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = BASE_DIR / 'emails'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
